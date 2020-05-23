b3dbcee2d6df6c3e5895b89029261b4d640469a8
becks045 <becks045@umn.edu>
2020-05-23 12:55:18 -0500

    retry

--- src/routes/getImages.js
+++ src/routes/getImages.js
@@ -8,9 +8,9 @@ var setChosen = false;
 
 
 router.get('/', function(req, res, next) {
-  var response = contents;
+  var response = dirPath;
   res.send(JSON.stringify(
-    {"cwd": dirPath}
+    {"cwd": response}
   ));
 });
 
