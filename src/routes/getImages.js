2afe7c18d066792a2d4b9e1876a95a1d12ed8b5e
becks045 <becks045@umn.edu>
2020-05-23 12:26:39 -0500

    Another try with functions/ + install, public in dist

--- src/routes/getImages.js
+++ src/routes/getImages.js
@@ -3,7 +3,7 @@ var path = require('path');
 var fs = require('fs');
 var router = express.Router();
 
-const dirPath = path.join('./', 'public');
+const dirPath = path.join('dist/', 'public');
 var setChosen = false;
 
 
