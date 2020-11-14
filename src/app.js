1aa27f9476f8314c55ef02b2c8846032ee70a99d
becks045 <becks045@umn.edu>
2020-11-14 14:35:28 -0600

    Adding websockets tosend chat messages

--- src/app.js
+++ src/app.js
@@ -10,7 +10,22 @@ var testAPIRouter = require('./routes/testAPI');
 var getImagesRouter = require('./routes/game');
 var uploadRouter = require('./routes/upload');
 
+const WebSocket = require('ws');
+
 const app = express();
+
+const wss = new WebSocket.Server({ port: 8080});
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+        console.log(data);
+      }
+    })
+  })
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
