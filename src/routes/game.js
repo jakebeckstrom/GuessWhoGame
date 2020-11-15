41d1e18114f77e3691688b1e22e10413d22ba149
becks045 <becks045@umn.edu>
2020-11-15 11:29:07 -0600

    removing unused imports

--- src/routes/game.js
+++ src/routes/game.js
@@ -1,8 +1,6 @@
 var express = require('express');
-var path = require('path');
 var router = express.Router();
 var AWS = require('aws-sdk');
-var dotenv = require('dotenv').config();
 
 
 var users = 0;
