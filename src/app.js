94ba5a8cbdb180f8bfd01237131de3730a2fcc08
becks045 <becks045@umn.edu>
2020-11-15 11:00:52 -0600

    Fixing ws server

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-var createError = require('http-errors');
 const express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -10,22 +9,7 @@ var testAPIRouter = require('./routes/testAPI');
 var getImagesRouter = require('./routes/game');
 var uploadRouter = require('./routes/upload');
 
-const WebSocket = require('ws');
-
 const app = express();
-
-const wss = new WebSocket.Server({ port: 8080});
-
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(data) {
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data);
-        console.log(data);
-      }
-    })
-  })
-})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -44,21 +28,5 @@ app.use('/testAPI', testAPIRouter);
 app.use('/game', getImagesRouter);
 app.use('/upload', uploadRouter);
 
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
