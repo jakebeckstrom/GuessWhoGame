231d03c7dc8d87748109671de0e347fc6fd3e6da
Jacob <jakebeckstrom@gmail.com>
2022-12-17 15:15:37 -0600

    Current config

--- src/app.js
+++ src/app.js
@@ -1,32 +1,31 @@
-const express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
-var bodyParser = require('body-parser');
+const express = require("express");
+var serverless = require("serverless-http");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
+var bodyParser = require("body-parser");
 
-var testAPIRouter = require('./routes/testAPI');
-var getImagesRouter = require('./routes/game');
-var uploadRouter = require('./routes/upload');
+var testAPIRouter = require("./routes/testAPI");
+var getImagesRouter = require("./routes/game");
+var uploadRouter = require("./routes/upload");
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/public', express.static(path.join('./', 'public')));
+app.use("/public", express.static(path.join("./", "public")));
 
-
-app.use('/testAPI', testAPIRouter);
-app.use('/game', getImagesRouter);
-app.use('/upload', uploadRouter);
-
+app.use("/testAPI", testAPIRouter);
+app.use("/game", getImagesRouter);
+app.use("/upload", uploadRouter);
 
 module.exports = app;
